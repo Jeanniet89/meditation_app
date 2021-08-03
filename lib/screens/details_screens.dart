0c26fc9f1c951a2d5614e4252d5890260b25b60c
Jeannie Torres <jeanniet89@gmail.com>
2021-08-03 08:36:43 -0400

    Sesssion cards

--- lib/screens/details_screens.dart
+++ lib/screens/details_screens.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:meditation_app/constants.dart';
 import 'package:meditation_app/widgets/search_bar.dart';
 
@@ -52,52 +53,111 @@ class DetailsScreen extends StatelessWidget {
               child: SearchBar(),
               ),
               Wrap(
+                spacing: 20,
+                runSpacing: 20,
                 children: <Widget>[
-                  Container(
-                    padding: EdgeInsets.all(16),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(13),
-                      boxShadow: [
-                        BoxShadow(
-                          offset: Offset(0,17),
-                          blurRadius: 23,
-                          spreadRadius: -13,
-                          color: kShadowColor,
-                      ),
-                      ],
-                      ),
-                      child: Row(
-                      children: <Widget>[
-                        Container(
-                          height: 42,
-                          width: 43,
-                          decoration: BoxDecoration(
-                            color: kBlueColor,
-                            shape: BoxShape.circle,
-                          ),
-                          child: Icon(
-                            Icons.play_arrow,
-                            color: Colors.white,
-                          ),
-                    ),
-                    SizedBox(width: 10,),
-                    Text(
-                      "Session 1",
-                      style: Theme.of(context)
-                      .textTheme
-                      .subtitle1,
-                      ),
+                  SessionCard(
+                    sessionNum: 1,
+                    isDone: true,
+                    press: () {},
+                  ),
+                   SessionCard(
+                    sessionNum: 2,
+                    press: () {},
+                  ),
+                   SessionCard(
+                    sessionNum: 3,
+                    press: () {},
+                  ),
+                   SessionCard(
+                    sessionNum: 4,
+                    press: () {},
+                  ),
+                   SessionCard(
+                    sessionNum: 5,
+                    press: () {},
+                  ),
+                   SessionCard(
+                    sessionNum: 6,
+                    press: () {},
+                  ),
                     ],
-                    )
                     ),
-                    ],
-                    )
         ],
         ),
           ),
         ),
       ],
     ),);
+  }
+}
+
+class SessionCard extends StatelessWidget {
+  final int sessionNum;
+  final bool isDone;
+  final Function press;
+  const SessionCard({
+    Key key, 
+    this.sessionNum, 
+    this.isDone = false, 
+    this.press,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraint) {
+        return ClipRRect(
+          borderRadius: BorderRadius.circular(13),
+          child: Container(
+            width: constraint.maxWidth / 2 - 10,
+            // padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(13),
+              boxShadow: [
+                BoxShadow(
+                  offset: Offset(0, 17),
+                  blurRadius: 23,
+                  spreadRadius: -13,
+                  color: kShadowColor,
+              ),
+              ],
+              ),
+              child: Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  onTap: press,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Row(
+                    children: <Widget>[
+                      Container(
+                        height: 42,
+                        width: 43,
+                        decoration: BoxDecoration(
+                          color: isDone ? kBlueColor : Colors.white,
+                          shape: BoxShape.circle,
+                          border: Border.all(color: kBlueColor)),
+                        child: Icon(
+                          Icons.play_arrow,
+                          color: isDone ? Colors.white : kBlueColor,
+                        ),
+            ),
+            SizedBox(width: 10,),
+            Text(
+                    "Session $sessionNum",
+                    style: Theme.of(context)
+                    .textTheme
+                    .subtitle2,
+                    ),
+            ],
+            ),
+                  ),
+                ),
+              )
+            ),
+        );
+      }
+    );
   }
 }
