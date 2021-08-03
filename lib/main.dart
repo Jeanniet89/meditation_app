8532b215de797e5d2171c30d4a2237dab947925a
Jeannie Torres <jeanniet89@gmail.com>
2021-08-03 01:59:48 -0400

    home category cards

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:meditation_app/category_card.dart';
 import 'package:meditation_app/constants.dart';
 
 void main() {
@@ -27,7 +29,23 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     // Gives us total height & width size of our device
+    var crossAxisAlignment;
     return Scaffold(
+      bottomNavigationBar: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        height: 70,
+        color: Colors.white,
+        child: Row(
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                SvgPicture.asset("assets/icons/calendar.svg"),
+                Text("Today"),
+              ],
+              )
+          ], 
+          ),
+      ),
       body: Stack(
         children: <Widget>[
           Container(
@@ -41,18 +59,82 @@ class HomeScreen extends StatelessWidget {
             ),
           ),
           SafeArea(
-            child: Column(children: <Widget>[
-              Container(
-                height: 52,
-                width: 52,
-                decoration: BoxDecoration(
-                  color: Color(0xFFF2EBA1)
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: crossAxisAlignment.start,
+                children: <Widget>[
+                Align(
+                  alignment: Alignment.topRight,
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 52,
+                    width: 52,
+                    decoration: BoxDecoration(
+                      color: Color(0xFFF2EBA1),
+                      shape: BoxShape.circle,
+                    ),
+                    child: SvgPicture.asset("assets/icons/menu.svg"),
+                  ),
                 ),
-              )
-            ]),
+                Text("Good morning \nSunshine",
+                style: Theme.of(context)
+                .textTheme
+                .headline4
+                .copyWith(fontWeight: FontWeight.w900),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 30),
+                  padding:EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+                  decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(29.5),
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: "Search",
+                      icon: SvgPicture.asset("assets/icons/search.svg"),
+                      border: InputBorder.none,
+                      ),
+                  ),
+                  ),
+                  Expanded(
+                    child: GridView.count(
+                      crossAxisCount: 2,
+                      childAspectRatio: .85,
+                      crossAxisSpacing: 20,
+                      mainAxisSpacing: 20,
+                      children: <Widget>[
+                        CategoryCard(
+                          title: "Diet Reconmendation",
+                          svgSrc: "assets/icons/Hamburger.svg",
+                          press: () {},
+                        ), 
+                          CategoryCard(
+                          title: "Kegel Exercises",
+                          svgSrc: "assets/icons/Excrecises.svg",
+                          press: () {},
+                        ), 
+                          CategoryCard(
+                          title: "Meditation",
+                          svgSrc: "assets/icons/Meditation.svg",
+                          press: () {},
+                        ),
+                           CategoryCard(
+                          title: "Yoga",
+                          svgSrc: "assets/icons/yoga.svg",
+                          press: () {},
+                        ),
+                     ],
+                     ),
+                  )
+              ],
+              ),
+            ),
           )
         ],
       ),
     );
   }
 }
+
